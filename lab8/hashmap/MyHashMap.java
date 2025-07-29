cbde9628c3516574b85c510d4ff5d824bd7505e9
SweetGargamel <295006525@qq.com>
2025-07-29 16:25:38 +0800

    first Verson of Hashmap

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,6 +1,9 @@
 package hashmap;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
@@ -10,7 +13,6 @@ import java.util.Collection;
  *  @author YOUR NAME HERE
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
-
     /**
      * Protected helper class to store key/value pairs
      * The protected qualifier allows subclass access
@@ -24,15 +26,22 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
             value = v;
         }
     }
-
+    private int  size;
+    private int capacity;
+    private double loadFactor;
     /* Instance Variables */
     private Collection<Node>[] buckets;
     // You should probably define some more!
-
+    private static int DEFAULT_CAPACITY = 16;
+    private static double DEFAULT_LOAD_FACTOR = 0.75;
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this(DEFAULT_CAPACITY,DEFAULT_LOAD_FACTOR);
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        this(initialSize,DEFAULT_LOAD_FACTOR);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialSize.
@@ -41,13 +50,18 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialSize initial size of backing array
      * @param maxLoad maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
+    public MyHashMap(int initialSize, double maxLoad) {
+        this.capacity=initialSize;
+        this.loadFactor=maxLoad;
+        this.size=0;
+        this.buckets= createTable(initialSize);
+    }
 
     /**
      * Returns a new node to be placed in a hash table bucket
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key,value);
     }
 
     /**
@@ -82,10 +96,110 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param tableSize the size of the table to create
      */
     private Collection<Node>[] createTable(int tableSize) {
+        Collection<Node>[] new_buckets = new Collection[tableSize];
+        for (int i = 0; i < tableSize; i++) {
+            new_buckets[i] = createBucket();
+        }
+        return new_buckets;
+    }
+
+
+    @Override
+    public void clear() {
+        this.buckets = createTable(DEFAULT_CAPACITY);
+        this.capacity=DEFAULT_CAPACITY;
+        this.size = 0;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+
+        return get(key) != null;
+    }
+    private int getIndexHelper(K key) {
+        int hash = key.hashCode();
+        int index = Math.floorMod(hash, capacity);
+        return index;
+    }
+    @Override
+    public V get(K key) {
+        int index = key.hashCode();
+        Collection bucket = buckets[index];
+        Iterator<Node> iterator = bucket.iterator();
+        Node node = null;
+        while(iterator.hasNext()) {
+            node = iterator.next();
+            if(node.key.equals(key)) {
+                return node.value;
+            }
+        }
         return null;
     }
 
-    // TODO: Implement the methods of the Map61B Interface below
-    // Your code won't compile until you do so!
+    @Override
+    public int size() {
+        return this.size;
+    }
 
+    @Override
+    public void put(K key, V value) {
+        checkResize();
+        int index = getIndexHelper(key);
+        Iterator<Node> iterator = buckets[index].iterator();
+        while(iterator.hasNext()) {
+            Node node = iterator.next();
+            if(node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+        }
+        buckets[index].add(createNode(key,value));
+        size++;
+
+    }
+    private void checkResize(){
+        //1 检查是否需要扩
+        if(this.size/this.capacity <= this.loadFactor){
+            return;
+        }
+        this.capacity*=2;//扩容
+        //创创建新的桶
+        Collection<Node>[] new_buckets = createTable(this.capacity);
+        for(int i = 0;i< this.buckets.length;i++){
+            //拿到旧的bucket
+            Collection<Node> bucket = this.buckets[i];
+            //把里面的东西重新算一遍hash然后复制
+            for(Node node : bucket){
+                int index = getIndexHelper(node.key);
+                new_buckets[index].add(node);
+            }
+        }
+        this.buckets = new_buckets;
+    }
+    @Override
+    public Set<K> keySet() {
+        Set<K> keySet = new HashSet<K>();
+        for(int i = 0;i< this.buckets.length;i++){
+            Collection<Node> bucket = this.buckets[i];
+            for(Node node : bucket){
+                keySet.add(node.key);
+            }
+        }
+        return keySet;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
 }
